0553e7efb478e6a04dd5421e994411ad50a8d78d
uditkumar489 <uditkumar489@gmail.com>
2017-11-07 07:55:51 +0530

    comns updated in LO_r
    comments updated

--- Regression/Logistics Regression/logistic_regression.py
+++ Regression/Logistics Regression/logistic_regression.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
+X = dataset.iloc[:, [2, 3]].values #selecting all rows + 3rd & 4th column from dataset
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
@@ -15,6 +15,8 @@ from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
+# for making the dataset observations comparable and predicting more accurate values
+# coz logi_reg don't take care of "FS" automatically unlike linear_reg
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
